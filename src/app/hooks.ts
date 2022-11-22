00b69eee2f67db987a3e360b8b361e0250969744
Yurii Kozlov <yurka22870@gmail.com>
2022-11-22 18:15:00 +0200

    intersection observer was added

---
+++ src/app/hooks.ts
@@ -0,0 +1,36 @@
+import {
+  useState, useEffect, MutableRefObject,
+} from 'react';
+
+export function useOnScreen<T extends Element>(
+  ref: MutableRefObject<T>, rootMargin = '0px',
+): boolean {
+  // State and setter for storing whether element is visible
+  const [isIntersecting, setIntersecting] = useState<boolean>(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        // Update our state when observer callback fires
+        setIntersecting(entry.isIntersecting);
+      },
+      {
+        rootMargin,
+      },
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    if (isIntersecting) {
+      observer.unobserve(ref.current);
+    }
+
+    return () => {
+      return observer.unobserve(ref.current);
+    };
+  }, [isIntersecting, setIntersecting]); // Empty array ensures that effect is only run on mount and unmount
+
+  return isIntersecting;
+}
--- src/components/Statistics/Statistics.tsx
+++ src/components/Statistics/Statistics.tsx
@@ -1,55 +1,30 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useEffect, useState } from 'react';
+import { useRef } from 'react';
 import { AnimatedNumbers } from '../AnimatedNumbers';
+import { useOnScreen } from '../../app/hooks';
 import './Statistics.scss';
 import './gridStatistics.scss';
 
 export const Statistics = () => {
-  const [areAnimatingNumbers, setAreAnimatingNumbers]
-  = useState<boolean>(false);
-  const [isOnDesktop, setIsOnDesktop] = useState<boolean>(true);
-
-  useEffect(() => {
-    window.addEventListener('resize', () => {
-      if (document.documentElement.clientWidth < 1024) {
-        setIsOnDesktop(false);
-      } else {
-        setIsOnDesktop(true);
-      }
-    });
-  });
-
-  const scrollHandler = (event:any) => {
-    if (event.target.documentElement.scrollTop > 7000 && isOnDesktop) {
-      setAreAnimatingNumbers(true);
-    }
-
-    if (event.target.documentElement.scrollTop > 10000 && !isOnDesktop) {
-      setAreAnimatingNumbers(true);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener('scroll', scrollHandler);
-
-    return () => {
-      return document.removeEventListener('scroll', scrollHandler);
-    };
-  }, []);
+  const ref: any = useRef<HTMLDivElement>();
+  const onScreen: boolean = useOnScreen<HTMLDivElement>(ref,
+    '100px');
 
   return (
     <section
       className="statistics"
     >
       <div className="container">
-        <div className="grid-statistics">
+        <div
+          className="grid-statistics"
+          ref={ref}
+        >
           <div className="grid-statistics__item">
             <div className="statistics__icon">
               <i className="fa-regular fa-pen-to-square" />
             </div>
             <div className="statistics__count">
-              {areAnimatingNumbers && <AnimatedNumbers n={1500} />}
-
+              {onScreen && <AnimatedNumbers n={1500} /> }
             </div>
             <div className="statistics__title">Projects completed</div>
           </div>
@@ -58,7 +33,7 @@ export const Statistics = () => {
               <i className="fa-regular fa-user" />
             </div>
             <div className="statistics__count">
-              {areAnimatingNumbers && <AnimatedNumbers n={900} />}
+              {onScreen && <AnimatedNumbers n={900} />}
             </div>
             <div className="statistics__title">Happy clients</div>
           </div>
@@ -67,7 +42,7 @@ export const Statistics = () => {
               <i className="fa-regular fa-id-badge" />
             </div>
             <div className="statistics__count">
-              {areAnimatingNumbers && <AnimatedNumbers n={200} />}
+              {onScreen && <AnimatedNumbers n={200} />}
             </div>
             <div className="statistics__title">Awards received</div>
           </div>
@@ -78,7 +53,7 @@ export const Statistics = () => {
                 <i className="fa-regular fa-lightbulb" />
               </div>
               <div className="statistics__count">
-                {areAnimatingNumbers && <AnimatedNumbers n={120} />}
+                {onScreen && <AnimatedNumbers n={120} /> }
               </div>
               <div className="statistics__title">Crazy ideas</div>
             </div>
@@ -89,7 +64,7 @@ export const Statistics = () => {
                 <i className="fa-solid fa-martini-glass-empty" />
               </div>
               <div className="statistics__count">
-                {areAnimatingNumbers && <AnimatedNumbers n={1500} />}
+                {onScreen && <AnimatedNumbers n={1500} />}
               </div>
               <div className="statistics__title">Coffee cups</div>
             </div>
@@ -100,7 +75,7 @@ export const Statistics = () => {
                 <i className="fa-regular fa-clock" />
               </div>
               <div className="statistics__count">
-                {areAnimatingNumbers && <AnimatedNumbers n={7200} />}
+                {onScreen && <AnimatedNumbers n={7200} /> }
               </div>
               <div className="statistics__title">Hours</div>
             </div>
